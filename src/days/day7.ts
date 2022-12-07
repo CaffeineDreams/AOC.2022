d78e2194795babb64bd9635368b5fc0f7a94e74e
Richard Milne <richard@caffeinedreams.co.uk>
2022-12-07 22:42:01 +0000

    files...

---
+++ src/days/day7.ts
@@ -0,0 +1,47 @@
+import { AocFunc } from "../helpers/AocHelpers";
+
+export  const day7: AocFunc = (input: string[]): string[] => {
+    
+    type treeNode = {
+        size?: number;
+    };
+
+    const tree = new Map<string, treeNode>([
+        ["/", {}]
+    ]);
+
+    const addNode = (path: string, node: treeNode) => {
+        tree[path] = node;
+    };
+
+    const buildTree = () => {
+        let currentPath: string;
+        
+        for (const instruction of input) {
+            const [dir] = instruction.match(/\$ cd (.+)/)?.slice(1) ?? [];
+            if (dir) {
+                currentPath = dir !== ".."
+                    ? dir
+                    : currentPath?.slice(0, currentPath.lastIndexOf("/")) ?? "/";
+                continue;
+            }
+            if (instruction === "$ ls")
+                continue;
+            
+            const [param, key] = instruction.split(" ");
+            switch (param){
+                case "dir":
+                    tree[`${currentPath}${key}/`] = {};
+                    break;
+                default:
+                    tree[`${currentPath}/${key}`] = { size: param};
+            }
+        }
+    }
+
+    buildTree();
+
+    console.log(tree);
+
+    return ["",""];
+}
