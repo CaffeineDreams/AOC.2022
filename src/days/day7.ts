fceb7a53fe1d88f030669f13325721194bb95cfa
Richard Milne <richard@caffeinedreams.co.uk>
2023-12-07 14:26:33 +0000

    ???

--- src/days/day7.ts
+++ src/days/day7.ts
@@ -7,12 +7,7 @@ export  const day7: AocFunc = (input: string[]): string[] => {
     };
 
     const tree = new Map<string, treeNode>([
-        ["/", {}]
     ]);
-
-    const addNode = (path: string, node: treeNode) => {
-        tree[path] = node;
-    };
 
     const buildTree = () => {
         let currentPath: string;
@@ -21,8 +16,8 @@ export  const day7: AocFunc = (input: string[]): string[] => {
             const [dir] = instruction.match(/\$ cd (.+)/)?.slice(1) ?? [];
             if (dir) {
                 currentPath = dir !== ".."
-                    ? dir
-                    : currentPath?.slice(0, currentPath.lastIndexOf("/")) ?? "/";
+                    ? dir + "/"
+                    : currentPath?.slice(0, currentPath.lastIndexOf("/")) ?? "" + "/";
                 continue;
             }
             if (instruction === "$ ls")
@@ -31,10 +26,10 @@ export  const day7: AocFunc = (input: string[]): string[] => {
             const [param, key] = instruction.split(" ");
             switch (param){
                 case "dir":
-                    tree[`${currentPath}${key}/`] = {};
+                    currentPath += key + "/";
                     break;
                 default:
-                    tree[`${currentPath}/${key}`] = { size: param};
+                    tree[`${currentPath}${key}`] = { size: param};
             }
         }
     }
